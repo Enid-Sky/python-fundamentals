e2e6a0d2314169762d7f8c1e127e5093fb633486
Enid-Sky <enid.sky@protonmail.com>
2021-02-02 16:01:43 -0800

    add Note

--- appendMethod.py
+++ appendMethod.py
@@ -1,9 +1,9 @@
 # Call .append() on an existing list to add a new item to the end
-# With append you can add integers, dictionaries, tuples, floating points etc.
-#
+# With append you can add integers, dictionaries, tuples, floating points, and any objects.
+# Python lists reserve extra space for new items at the end of the list. A call to .append() will place new items in the available space.
 
 
-mixed = [1, 2]
+mixed = [1, 2]  # initial list
 
 mixed.append(3)  # integer
 
@@ -16,3 +16,14 @@ mixed.append({"greeting": "hello"})  # dictionary
 mixed.append((1, "hello", 5.0))  # tuple
 
 print(f'mixed: {mixed}')
+
+
+# NOTE: Using append is equivalent to the following operation
+
+# SLICE OPERATION:
+
+numbers = [1, 2, 3]
+
+numbers[len(numbers):] = 4
+
+print(f'numbers: {numbers}')
